ac4188276a8874c99716b9b7face643b393bbd47
Bharadwaj <yajamankr@gmail.com>
2020-05-03 15:33:30 +0530

    Updates

---
+++ precache-manifest.bf3008af509725b86f4abbabf86f40ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fad1fb8863aec674d83ac3255f51f37",
+    "url": "/React-Todo-App/index.html"
+  },
+  {
+    "revision": "de51ed6b2f59319133d7",
+    "url": "/React-Todo-App/static/css/main.348c9207.chunk.css"
+  },
+  {
+    "revision": "b629a6f5c49906f3daf9",
+    "url": "/React-Todo-App/static/js/2.dd32704b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todo-App/static/js/2.dd32704b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de51ed6b2f59319133d7",
+    "url": "/React-Todo-App/static/js/main.970798e3.chunk.js"
+  },
+  {
+    "revision": "8ea558db588aaf0fa6d8",
+    "url": "/React-Todo-App/static/js/runtime-main.e6395246.js"
+  }
+]);
